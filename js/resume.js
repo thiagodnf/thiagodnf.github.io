161b3132892072e53202c52a37c20a8641ba3edb
Thiago Nascimento <thiagodnf@gmail.com>
2020-03-05 23:16:34 -0500

    added # to url

--- js/resume.js
+++ js/resume.js
@@ -1,16 +1,20 @@
 (function($) {
-  
+
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
+      var name = target;
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top)
         }, "slow");
+
+        window.location.hash = $(name).attr("id");
+
         return false;
       }
     }
