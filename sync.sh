5f9e6ad54acaef8ef1279e4d827d7589e6b8f837
Thiago Nascimento <thiagodnf@gmail.com>
2020-10-26 23:26:46 -0400

    added support to nodejs

--- sync.sh
+++
@@ -1,9 +0,0 @@
-echo "Sincronizando arquivos com o servidor"
-
-rsync -ravzup dist/tnferreira/* tnferreira@ssh.inf.ufpr.br:~/tnferreira/
-
-echo "Removendo pasta build"
-
-rm -rf build
-
-echo "Feito"
