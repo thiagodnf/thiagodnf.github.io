cdded121ca937417049f8bae7454983b42d4ae4f
Thiago Ferreira <thiagodnf@gmail.com>
2021-04-26 23:45:23 -0400

    added support to dark mode

--- js/resume.js
+++ js/resume.js
@@ -30,4 +30,19 @@
     target: '#sideNav'
   });
 
+
+  $('#theme-toggle').click(function() {
+
+    document.body.classList.toggle('dark-theme');
+    // // If the OS is set to dark mode...
+    // if (prefersDarkScheme.matches) {
+    //   // ...then apply the .light-theme class to override those styles
+    //   document.body.classList.toggle("light-theme");
+    //   // Otherwise...
+    // } else {
+    //   // ...apply the .dark-theme class to override the default light styles
+    //   document.body.classList.toggle("dark-theme");
+    // }
+  });
+
 })(jQuery); // End of use strict
